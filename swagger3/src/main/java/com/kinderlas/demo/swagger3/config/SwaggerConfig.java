795bbb7619b7c20fc3fcf1dd04db6c12d6e22094
Kinderlas <Kinderlas@gmail.com>
2021-07-07 23:01:19 +0800

    swagger3: 加入全局参数

--- swagger3/src/main/java/com/kinderlas/demo/swagger3/config/SwaggerConfig.java
+++ swagger3/src/main/java/com/kinderlas/demo/swagger3/config/SwaggerConfig.java
@@ -1,15 +1,23 @@
 package com.kinderlas.demo.swagger3.config;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.parameters.Parameter;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -23,7 +31,9 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo())
                 .select()
                 .build()
-                .securitySchemes(security());
+                .globalRequestParameters(getGlobalRequestParameters())
+                ;
+                // .securitySchemes(security());
     }
 
     private List<SecurityScheme> security() {
@@ -38,6 +48,36 @@ public class SwaggerConfig {
                 .contact(new Contact("kinderlas", "http://www.kinderlas.com", "kinderlas@gmail.com"))
                 .version("1.0")
                 .build();
+    }
+
+    //生成全局通用参数
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("appid")
+                .description("平台id(全局)")
+                .required(true)
+                .in(ParameterType.QUERY)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+        parameters.add(new RequestParameterBuilder()
+                .name("udid")
+                .description("设备的唯一id(全局)")
+                .required(true)
+                .in(ParameterType.QUERY)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+        parameters.add(new RequestParameterBuilder()
+                .name("version")
+                .description("客户端的版本号(全局)")
+                .required(true)
+                .in(ParameterType.QUERY)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+        return parameters;
     }
 
 }
