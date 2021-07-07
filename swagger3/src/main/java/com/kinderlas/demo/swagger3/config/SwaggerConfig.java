ac22f7a015e9da4c4bab02e352cffc00cf2d01d7
Kinderlas <Kinderlas@gmail.com>
2021-07-07 21:27:28 +0800

    swagger3: 标题/版本/auth 相关配置

---
+++ swagger3/src/main/java/com/kinderlas/demo/swagger3/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.kinderlas.demo.swagger3.config;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .build()
+                .securitySchemes(security());
+    }
+
+    private List<SecurityScheme> security() {
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        return Collections.singletonList(apiKey);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger3 demo 接口文档")
+                .description("demo")
+                .contact(new Contact("kinderlas", "http://www.kinderlas.com", "kinderlas@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
