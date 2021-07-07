d145087d9da6d2b8fdbbd547cb56a9661f158fc2
Kinderlas <Kinderlas@gmail.com>
2021-07-07 23:01:42 +0800

    swagger3: 尝试enum类型

--- swagger3/src/main/java/com/kinderlas/demo/swagger3/controller/HelloController.java
+++ swagger3/src/main/java/com/kinderlas/demo/swagger3/controller/HelloController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kinderlas.demo.swagger3.controller.HelloController.HelloDTO.Type;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -36,33 +38,48 @@ public class HelloController {
         String msg;
         @ApiModelProperty("状态")
         int status;
+
     }
 
     @Data
     @ApiModel("用户信息")
-    @ApiResponses({
-            @ApiResponse(code = 403, message = "无权限"),
-            @ApiResponse(code = 404, message = "内部错误"),
-    })
     public static class HelloDTO {
+
         @ApiModelProperty("姓名")
         String name;
+
         @ApiModelProperty("年龄")
         int age;
+
         @ApiModelProperty("哈哈")
         List<Integer> t;
+
+        Type type;
+
+        @ApiModel("用户类型")
+        public enum Type {
+            @ApiModelProperty("vip用户")
+            vip,
+            @ApiModelProperty("普通用户")
+            normal
+        }
     }
 
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", value = "用户id", required = true, paramType = "path"),
             @ApiImplicitParam(name = "forward", value = "ip", required = true, paramType = "header"),
     })
+    @ApiResponses({
+            @ApiResponse(code = 403, message = "无权限"),
+            @ApiResponse(code = 404, message = "内部错误"),
+    })
     @ApiOperation("hello get方法")
     @GetMapping("/hello/{id}")
     public Response hello(
             @PathVariable Integer id,
             @ApiParam(value = "姓名", required = true) String name,
-            @ApiParam(value = "年龄") @RequestParam(defaultValue = "12") String age
+            @ApiParam(value = "年龄") @RequestParam(defaultValue = "12") String age,
+            @ApiParam("用户类型") @RequestParam(defaultValue = "vip") Type type
     ) {
         Response response = new Response();
         response.setData("hello " + name + ":" + age);
