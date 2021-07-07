d2c462a7f24a72ffc7166b08b97803fb373ed879
Kinderlas <Kinderlas@gmail.com>
2021-07-07 21:35:03 +0800

    swagger3: 更复杂的get/post请求

--- swagger3/src/main/java/com/kinderlas/demo/swagger3/controller/HelloController.java
+++ swagger3/src/main/java/com/kinderlas/demo/swagger3/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.kinderlas.demo.swagger3.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +19,27 @@ public class HelloController {
         int status;
     }
 
+    @Data
+    public static class HelloDTO {
+        String name;
+        int age;
+    }
+
     @GetMapping("/hello")
-    public Response hello() {
+    public Response hello(String name) {
         Response response = new Response();
-        response.setData("hello");
+        response.setData("hello " + name);
+        response.setMsg("success");
+        response.setStatus(0);
+        return response;
+    }
+
+    @PostMapping("/hello")
+    public Response helloPost(
+            @RequestBody HelloDTO helloDTO
+    ) {
+        Response response = new Response();
+        response.setData("hello " + helloDTO.name + ":" + helloDTO.age);
         response.setMsg("success");
         response.setStatus(0);
         return response;
